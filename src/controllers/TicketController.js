03a2c91537d369d9057cd29730bda442ed74593c
escobarlep <lucasescperes@gmail.com>
2021-03-11 02:10:48 -0300

    refactor: rename history type to complexity

--- src/controllers/TicketController.js
+++ src/controllers/TicketController.js
@@ -29,20 +29,20 @@ export default {
     this._partnerRepo.setStorage(this._mainApp.storage)
     this._templateRepo.setStorage(this._mainApp.storage)
   },
-  getTypeFromView: function() {
-    const type = this._mainApp.document.querySelectorAll(this.viewMain.classSelectType)
-    let selectedType 
-    type.forEach(element => {
-      if (element.checked) selectedType = element.getAttribute('data-type')
+  getComplexityFromView: function() {
+    const complexity = this._mainApp.document.querySelectorAll(this.viewMain.classSelectComplexity)
+    let selectedComplexity 
+    complexity.forEach(element => {
+      if (element.checked) selectedComplexity = element.getAttribute('data-complexity')
     })
-    return selectedType ? selectedType : 'medium'
+    return selectedComplexity ? selectedComplexity : 'medium'
   },
   collectAndSaveTicket: function() {
     const customer = this._customerRepo.getCustomer()
     const partner = this._partnerRepo.getPartner()
     const template = this._templateRepo.getLastUsedTempl()
-    const type = this.getTypeFromView()
-    this._repo.add({ customer, partner, template, type })
+    const complexity = this.getComplexityFromView()
+    this._repo.add({ customer, partner, template, complexity })
     const btnClearCustomer = this._mainApp.document.getElementById(this.viewCustomer.idBtnClear)
     const btnClearPartner = this._mainApp.document.getElementById(this.viewPartner.idBtnClear)
     this._templateRepo.clearHistory()
